62b33f5d471a0788cbfad1562b6e33475d289bfd
Yeosuwacos <yeosuwacos@gmail.com>
2026-05-03 01:55:40 -0400

    Main menu and transition system

--- eientei-arcade/scenes/main_menu/main_menu.gd
+++ eientei-arcade/scenes/main_menu/main_menu.gd
@@ -1,11 +1,4 @@
-extends Node2D
+extends Control
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_btn_start_pressed() -> void:
+	SceneTransition.go_to("res://scenes/game_select/GameSelect.tscn")
