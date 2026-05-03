6cee6c2411bb7eec7e6ff41f4219c6e074464034
Yeosuwacos <yeosuwacos@gmail.com>
2026-05-03 01:35:16 -0400

    Display, autoloads and ticket signal

--- eientei-arcade/scripts/common/MinigameBase.gd
+++ eientei-arcade/scripts/common/MinigameBase.gd
@@ -1,11 +1,10 @@
+class_name MinigameBase
 extends Node
 
+signal game_finished(tickets_won)
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func start_game():
 	pass
+
+func end_game(tickets_won):
+	game_finished.emit(tickets_won)
