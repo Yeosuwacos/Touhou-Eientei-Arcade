66997729572e296ea315e785575936a01336dbc3
Yeosuwacos <yeosuwacos@gmail.com>
2026-05-03 22:54:53 -0400

    Options & connecting buttons

--- eientei-arcade/scenes/main_menu/main_menu.gd
+++ eientei-arcade/scenes/main_menu/main_menu.gd
@@ -2,3 +2,6 @@ extends Control
 
 func _on_btn_start_pressed() -> void:
 	SceneTransition.go_to("res://scenes/game_select/GameSelect.tscn")
+
+func _on_btn_options_pressed() -> void:
+	SceneTransition.go_to("res://scenes/options/Options.tscn")
