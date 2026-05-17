ac6c311e20036ef2180432214ef7e54260773782
Yeosuwacos <yeosuwacos@gmail.com>
2026-05-17 00:07:12 -0400

    Play again option on game end

--- eientei-arcade/scenes/win_lose/win_lose.gd
+++ eientei-arcade/scenes/win_lose/win_lose.gd
@@ -1,5 +1,11 @@
 extends Control
 
+@export var current_scene: String
+
 #Return to game selection screen
 func _on_btn_back_pressed() -> void:
 	SceneTransition.go_to("res://scenes/game_select/GameSelect.tscn")
+
+#Play again
+func _on_btn_again_pressed() -> void:
+	SceneTransition.go_to(current_scene)
